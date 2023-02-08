811101ec84cbc342bdf6eaec35e5c9a203d365bc
Daniel <dscohen93@gmail.com>
2023-02-08 13:30:08 +0200

    init

---
+++ AbraUserApi/HttpRequests.cs
@@ -0,0 +1,18 @@
+﻿namespace AbraUserApi
+{
+    public class HttpRequest
+    {
+        private static HttpClient sharedClient = new()
+        {
+            BaseAddress = new Uri("https://randomuser.me/api/"),
+        };
+
+        static async Task<HttpResponseMessage> GetUserDataAsync(HttpClient httpClient, string gender)
+        {
+            using HttpResponseMessage response = await httpClient.GetAsync($"?results=10&gender={gender}");
+
+            return response;
+
+        }
+    }
+}
